332ae0c11b5828319f7a709c8ef0498b7337f829
Akash Regmi <akashregmi82@gmail.com>
2025-05-05 19:36:54 +0545

    making the navbar in process....

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route,  Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Iproducts from "./pages/Iproducts";
 import Cart from "./pages/Cart";
 import Contact from "./pages/Contact";
 import Error from "./pages/Error";
+
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+
           <Route path="/products" element={<Products />} />
           <Route path="/products/:id" element={<Iproducts />} />
           <Route path="/cart" element={<Cart />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,37 @@
 import { IoMdCart } from "react-icons/io";
+import { IoMdSearch } from "react-icons/io";
+import Logo from "../assets/AkashMart.jpg"
 function  Navbar () {
     return(
 
         <nav className="bg-white shadow-md px-6 py-4 flex items-center justify-between">
         {/* Logo */}
-        <div className="text-2xl font-bold text-teal-700">AkashMart
         
+         <button className=" cursor-pointer transition-transform duration-300 hover:scale-105"><img src={Logo} alt="AkashMart logo" className="h-20 rounded-2xl"/></button> 
         
-        </div>
-  
         {/* Search Bar */}
         <div className="flex-1 mx-6">
+        
           <input
+          
             type="text"
             placeholder="Search products..."
             className="w-full border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-teal-500"
+            
           />
+          
         </div>
   
-        // this is for the cart icon 
+        
         <div className="relative cursor-pointer">
          
           <IoMdCart className="h-6 w-6 text-gray-700" />
           <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-1.5 py-0.5 rounded-full">
             0
           </span>
+        </div>
+        <div>
+        <button  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ml-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Login</button>
         </div>
       </nav>
     )
